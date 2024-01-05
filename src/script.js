f459a6d773247683049d71b21e71851db27f9477
kndxiu <95412042+kndxiu@users.noreply.github.com>
2024-01-05 20:48:04 +0100

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -2,14 +2,10 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "dat.gui";
-
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import gsap from "gsap";
-
 import { Power3 } from "gsap";
-
 import Howler from "howler";
-
 const gsaptl = gsap.timeline();
 
 const fileLoader = document.getElementById("loader");
@@ -24,6 +20,23 @@ const canvas = document.querySelector("canvas.webgl");
 
 const scene = new THREE.Scene();
 scene.fog = new THREE.Fog(0x000000, 0, 3.5);
+
+let volume = 0.5;
+
+const slider = document.getElementById("volume");
+const val = document.getElementById("val");
+
+slider.addEventListener("input", () => {
+  volume = slider.value / 100;
+  val.innerHTML = slider.value;
+});
+
+let camera;
+let renderer;
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
 const notes = [
   "A0",
@@ -116,105 +129,166 @@ const notes = [
   "C8",
 ];
 
+const keyboard = {};
+const keyboardOld = {};
+
+const keyMap = {
+  "Q": "C4",
+  "2": "Db4",
+  "W": "D4",
+  "3": "Eb4",
+  "E": "E4",
+  "R": "F4",
+  "5": "Gb4",
+  "T": "G4",
+  "6": "Ab4",
+  "Y": "A4",
+  "7": "Bb4",
+  "U": "B4",
+  "I": "C5",
+  "9": "Db5",
+  "O": "D5",
+  "0": "Eb5",
+  "P": "E5",
+  "Z": "F5",
+  "S": "Gb5",
+  "X": "G5",
+  "D": "Ab5",
+  "C": "A5",
+  "V": "Bb5",
+  "F": "B5",
+  "B": "C6",
+  "H": "Db6",
+  "N": "D6",
+  "J": "Eb6",
+  "M": "E6",
+};
+
 const audioSources = {};
 
 let piano;
 
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
+const setupCamera = () => {
+  camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    100
+  );
+  camera.position.x = 1.5;
+  camera.position.y = 2;
+  camera.position.z = 1.5;
+  camera.rotation.set(-0.7, 0.6, 0.5);
+  scene.add(camera);
+  const listener = new THREE.AudioListener();
+  camera.add(listener);
 };
 
-window.addEventListener("resize", () => {
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
+const setupLights = () => {
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 4);
+  directionalLight.position.x = 1;
+  directionalLight.position.y = 1;
+  directionalLight.position.z = 1;
+  scene.add(directionalLight);
+  const directionalLight1 = new THREE.DirectionalLight(0xffffff, 4);
+  directionalLight1.position.x = -1;
+  directionalLight1.position.y = 1;
+  directionalLight1.position.z = 1;
+  scene.add(directionalLight1);
+  const directionalLight2 = new THREE.DirectionalLight(0xffffff, 3);
+  directionalLight2.position.x = 0;
+  directionalLight2.position.y = 2;
+  directionalLight2.position.z = 1;
+  scene.add(directionalLight2);
 
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
+  const ambientLight = new THREE.AmbientLight(0xffffff, -3);
+  scene.add(ambientLight);
+};
 
+const setupBasicScene = () => {
+  const tl = new THREE.TextureLoader();
+
+  const aoMap = tl.load("textures/vbooccvew_2K_AO.jpg");
+  aoMap.wrapS = THREE.RepeatWrapping;
+  aoMap.wrapT = THREE.RepeatWrapping;
+  aoMap.repeat.set(10, 10);
+  const displacementMap = tl.load("textures/vbooccvew_2K_Displacement.jpg");
+  displacementMap.wrapS = THREE.RepeatWrapping;
+  displacementMap.wrapT = THREE.RepeatWrapping;
+  displacementMap.repeat.set(10, 10);
+  const normalMap = tl.load("textures/vbooccvew_2K_Normal.jpg");
+  normalMap.wrapS = THREE.RepeatWrapping;
+  normalMap.wrapT = THREE.RepeatWrapping;
+  normalMap.repeat.set(10, 10);
+  const roughnessMap = tl.load("textures/vbooccvew_2K_Roughness.jpg");
+  roughnessMap.wrapS = THREE.RepeatWrapping;
+  roughnessMap.wrapT = THREE.RepeatWrapping;
+  roughnessMap.repeat.set(10, 10);
+  const map = tl.load("textures/vbooccvew_2K_Albedo.jpg");
+  map.wrapS = THREE.RepeatWrapping;
+  map.wrapT = THREE.RepeatWrapping;
+  map.repeat.set(10, 10);
+
+  const geometry = new THREE.PlaneGeometry(10, 10);
+  const material = new THREE.MeshStandardMaterial({
+    color: 0x000000,
+    side: THREE.DoubleSide,
+    aoMap: aoMap,
+    displacementMap: displacementMap,
+    normalMap: normalMap,
+    roughnessMap: roughnessMap,
+    map: map,
+  });
+  const plane = new THREE.Mesh(geometry, material);
+  plane.rotation.x = THREE.Math.degToRad(90);
+  scene.add(plane);
+};
+
+const setupRenderer = () => {
+  renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true,
+  });
   renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
+  renderer.setPixelRatio(2);
+};
 
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100
-);
-camera.position.x = 1.5;
-camera.position.y = 2;
-camera.position.z = 1.5;
-camera.rotation.set(-0.7, 0.6, 0.5);
-// camera.position.x = 0;
-// camera.position.y = 1.25;
-// camera.position.z = 1;
-// camera.rotation.x = THREE.Math.degToRad(-15);
-scene.add(camera);
-const listener = new THREE.AudioListener();
-camera.add(listener);
+const setupEvents = () => {
+  window.addEventListener("resize", () => {
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
 
-// Lights
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 4);
-directionalLight.position.x = 1;
-directionalLight.position.y = 1;
-directionalLight.position.z = 1;
-scene.add(directionalLight);
-const directionalLight1 = new THREE.DirectionalLight(0xffffff, 4);
-directionalLight1.position.x = -1;
-directionalLight1.position.y = 1;
-directionalLight1.position.z = 1;
-scene.add(directionalLight1);
-const directionalLight2 = new THREE.DirectionalLight(0xffffff, 3);
-directionalLight2.position.x = 0;
-directionalLight2.position.y = 2;
-directionalLight2.position.z = 1;
-scene.add(directionalLight2);
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(2);
+  });
+  if (navigator.maxTouchPoints) {
+    document.addEventListener("touchstart", onTouchStart, false);
+    document.addEventListener("touchend", onTouchEnd, false);
+    document.addEventListener("touchmove", onTouchMove, false);
+  } else {
+    window.addEventListener("mousedown", onMouseDown, false);
+    window.addEventListener("mouseup", onMouseUp, false);
+    window.addEventListener("mousemove", onMouseMove, false);
+  }
 
-const ambientLight = new THREE.AmbientLight(0xffffff, -3);
-scene.add(ambientLight);
+  document.addEventListener("keydown", onKeyPress);
+  document.addEventListener("keyup", onKeyRelease);
+};
 
-const tl = new THREE.TextureLoader();
+const playSound = (name) => {
+  const sound = audioSources[name];
+  const soundId = sound.play();
+  sound.fade(0, volume, 50, soundId);
+  sound.fade(volume, 0, 3000);
+};
 
-const aoMap = tl.load("textures/vbooccvew_2K_AO.jpg");
-aoMap.wrapS = THREE.RepeatWrapping;
-aoMap.wrapT = THREE.RepeatWrapping;
-aoMap.repeat.set(10, 10);
-const displacementMap = tl.load("textures/vbooccvew_2K_Displacement.jpg");
-displacementMap.wrapS = THREE.RepeatWrapping;
-displacementMap.wrapT = THREE.RepeatWrapping;
-displacementMap.repeat.set(10, 10);
-const normalMap = tl.load("textures/vbooccvew_2K_Normal.jpg");
-normalMap.wrapS = THREE.RepeatWrapping;
-normalMap.wrapT = THREE.RepeatWrapping;
-normalMap.repeat.set(10, 10);
-const roughnessMap = tl.load("textures/vbooccvew_2K_Roughness.jpg");
-roughnessMap.wrapS = THREE.RepeatWrapping;
-roughnessMap.wrapT = THREE.RepeatWrapping;
-roughnessMap.repeat.set(10, 10);
-const map = tl.load("textures/vbooccvew_2K_Albedo.jpg");
-map.wrapS = THREE.RepeatWrapping;
-map.wrapT = THREE.RepeatWrapping;
-map.repeat.set(10, 10);
-
-const geometry = new THREE.PlaneGeometry(10, 10);
-const material = new THREE.MeshStandardMaterial({
-  color: 0x000000,
-  side: THREE.DoubleSide,
-  aoMap: aoMap,
-  displacementMap: displacementMap,
-  normalMap: normalMap,
-  roughnessMap: roughnessMap,
-  map: map,
-});
-const plane = new THREE.Mesh(geometry, material);
-plane.rotation.x = THREE.Math.degToRad(90);
-scene.add(plane);
+setupLights();
+setupBasicScene();
+setupCamera();
+setupRenderer();
 
 const loader = new GLTFLoader();
 
@@ -248,19 +322,22 @@ loader.load(
 
     Promise.all(
       notes.map((note, index) => {
-        loadingProgress.value = parseInt((index / 88) * 100);
-        currentFile.innerHTML = `sounds/${note}.mp3<span>${
-          index + 1
-        } / 88</span>`;
-        if (index + 1 == 88) {
-          currentFile.innerHTML = `Finishing loading...`;
-        }
         return new Promise((resolve) => {
-          audioSources[note] = new Howl({
+          const sound = (audioSources[note] = new Howl({
             src: [`sounds/${note}.mp3`],
-            volume: 1,
-            onload: resolve,
-          });
+            onload: () => {
+              if (sound.state() == "loaded") {
+                loadingProgress.value = parseInt((index / 88) * 100);
+                currentFile.innerHTML = `sounds/${note}.mp3<span>${
+                  index + 1
+                } / 88</span>`;
+                if (index + 1 == 88) {
+                  currentFile.innerHTML = `Finishing loading...`;
+                }
+                resolve();
+              }
+            },
+          }));
         });
       })
     ).then(() => {
@@ -274,7 +351,7 @@ loader.load(
   }
 );
 
-function filesLoaded() {
+const filesLoaded = () => {
   document.getElementById("view").addEventListener("click", () => {
     document.getElementById("view1").classList.remove("currentView");
     document.getElementById("view").classList.add("currentView");
@@ -391,43 +468,97 @@ function filesLoaded() {
       document.querySelector(".info").remove();
     }, 400);
   }, 5000);
-}
+};
 
-function getNote(string) {
+const getNote = (string) => {
   if (string[string.length - 2] === "0") {
     return string[string.length - 1];
   } else {
     return string.slice(-2);
   }
-}
+};
 
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
 
 let activeKey = null;
 
-function onMouseDown(event) {
+const pressKey = (object) => {
+  const baseRotation = object.userData.rotation;
+  const basePosition = object.userData.position;
+  object.rotation.x = baseRotation + THREE.Math.degToRad(5);
+  object.position.y = basePosition - 0.00475;
+};
+
+const releaseKey = (object) => {
+  const baseRotation = object.userData.rotation;
+  const basePosition = object.userData.position;
+  object.rotation.x = baseRotation;
+  object.position.y = basePosition;
+};
+
+const handleKeys = () => {
+  for (const key in keyboard) {
+    const keyNew = keyboard[key];
+    const keyOld = keyboardOld[key];
+
+    if (keyNew != keyOld) {
+      const keyName = key.toUpperCase();
+      const note = keyMap[keyName];
+      if (keyNew) {
+        try {
+          playSound(note);
+          const id = notes.indexOf(note);
+          const key = piano.children[0].getObjectByName(
+            `key${id.toString().padStart(2, 0)}`
+          );
+          pressKey(key);
+        } catch {}
+      }
+      if (keyOld) {
+        const id = notes.indexOf(note);
+        const key = piano.children[0].getObjectByName(
+          `key${id.toString().padStart(2, 0)}`
+        );
+        if (key) {
+          releaseKey(key);
+        }
+      }
+    }
+  }
+  Object.assign(keyboardOld, keyboard);
+};
+
+const onKeyPress = (event) => {
+  keyboard[event.key] = true;
+  handleKeys();
+};
+
+const onKeyRelease = (event) => {
+  keyboard[event.key] = null;
+  handleKeys();
+};
+
+const onMouseDown = (event) => {
   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
   raycaster.setFromCamera(mouse, camera);
   const intersects = raycaster.intersectObjects(pianoKeys);
   if (intersects.length > 0) {
     activeKey = intersects[0].object;
-    audioSources[notes[getNote(activeKey.name)]].play();
-    activeKey.position.y -= 0.005;
-    activeKey.rotation.x += 3 * THREE.Math.DEG2RAD;
+    const id = getNote(activeKey.name);
+    const name = notes[id];
+    playSound(name);
+    pressKey(activeKey);
   }
-}
+};
 
-function onMouseUp(event) {
-  if (activeKey) {
-    activeKey.position.y = activeKey.userData.position;
-    activeKey.rotation.x = activeKey.userData.rotation;
-    activeKey = null;
-  }
-}
+const onMouseUp = () => {
+  if (activeKey) releaseKey(activeKey);
+  activeKey = null;
+};
 
-function onMouseMove(event) {
+const onMouseMove = (event) => {
   if (!activeKey) return;
   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
@@ -435,18 +566,18 @@ function onMouseMove(event) {
   const intersects = raycaster.intersectObjects(pianoKeys);
   if (intersects.length > 0) {
     const key = intersects[0].object;
-    if (key !== activeKey) {
-      audioSources[notes[getNote(activeKey.name)]].play();
-      activeKey.position.y = activeKey.userData.position;
-      activeKey.rotation.x = activeKey.userData.rotation;
+    if (activeKey != key) {
+      releaseKey(activeKey);
       activeKey = key;
-      activeKey.position.y -= 0.005;
-      activeKey.rotation.x += 3 * THREE.Math.DEG2RAD;
+      const id = getNote(key.name);
+      const name = notes[id];
+      playSound(name);
+      pressKey(activeKey);
     }
   }
-}
+};
 
-function onTouchStart(event) {
+const onTouchStart = (event) => {
   const touch = event.touches[0];
   mouse.x = (touch.clientX / window.innerWidth) * 2 - 1;
   mouse.y = -(touch.clientY / window.innerHeight) * 2 + 1;
@@ -454,21 +585,19 @@ function onTouchStart(event) {
   const intersects = raycaster.intersectObjects(pianoKeys);
   if (intersects.length > 0) {
     activeKey = intersects[0].object;
-    audioSources[notes[getNote(activeKey.name)]].play();
-    activeKey.position.y -= 0.005;
-    activeKey.rotation.x += 3 * THREE.Math.DEG2RAD;
+    const id = getNote(activeKey.name);
+    const name = notes[id];
+    playSound(name);
+    pressKey(activeKey);
   }
-}
+};
 
-function onTouchEnd(event) {
-  if (activeKey) {
-    activeKey.position.y = activeKey.userData.position;
-    activeKey.rotation.x = activeKey.userData.rotation;
-    activeKey = null;
-  }
-}
+const onTouchEnd = () => {
+  if (activeKey) releaseKey(activeKey);
+  activeKey = null;
+};
 
-function onTouchMove(event) {
+const onTouchMove = (event) => {
   if (!activeKey) return;
   const touch = event.touches[0];
   mouse.x = (touch.clientX / window.innerWidth) * 2 - 1;
@@ -477,48 +606,23 @@ function onTouchMove(event) {
   const intersects = raycaster.intersectObjects(pianoKeys);
   if (intersects.length > 0) {
     const key = intersects[0].object;
-    if (key !== activeKey) {
-      audioSources[notes[getNote(activeKey.name)]].play();
-      activeKey.position.y = activeKey.userData.position;
-      activeKey.rotation.x = activeKey.userData.rotation;
+    if (activeKey != key) {
+      releaseKey(activeKey);
       activeKey = key;
-      activeKey.position.y -= 0.005;
-      activeKey.rotation.x += 3 * THREE.Math.DEG2RAD;
+      const id = getNote(key.name);
+      const name = notes[id];
+      playSound(name);
+      pressKey(activeKey);
     }
   }
-}
+};
 
-if (navigator.maxTouchPoints) {
-  document.addEventListener("touchstart", onTouchStart, false);
-  document.addEventListener("touchend", onTouchEnd, false);
-  document.addEventListener("touchmove", onTouchMove, false);
-} else {
-  window.addEventListener("mousedown", onMouseDown, false);
-  window.addEventListener("mouseup", onMouseUp, false);
-  window.addEventListener("mousemove", onMouseMove, false);
-}
-
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-  antialias: true,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+setupEvents();
 
 const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
-
-  // Update Orbital Controls
-  // controls.update()
 
   renderer.render(scene, camera);
 
